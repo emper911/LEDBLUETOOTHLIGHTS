6f7f30bf2f33918a94d567097154fde86304f864
emper911 <emper96@gmail.com>
2017-01-28 23:53:52 -0500

    Update BLUETOOTH_RGB_LEDstrip.ino

--- BLUETOOTH_RGB_LEDstrip.ino
+++ BLUETOOTH_RGB_LEDstrip.ino
@@ -16,69 +16,29 @@ void setup()
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
   pinMode(POWERPIN, OUTPUT);
-  digitalWrite(POWERPIN, HIGH);
+  digitalWrite(POWERPIN, HIGH);//sets relay to off
   analogWrite(REDPIN, r);
   analogWrite(GREENPIN, g);
   analogWrite(BLUEPIN, b);
 }
 
-/*
-void party(){
-  if(partyMode == 'r'){
-    for(int r = 0; r < 256; r++){
-      analogWrite(REDPIN, r);
-      delay(FADESPEED);
-    }
-    partyMode = 'b';
-  }
-  else if(partyMode == 'b'){
-    for(int b = 255; b > 0; b--){
-      analogWrite(BLUEPIN, b);
-      delay(FADESPEED);
-    }
-    partyMode = 'g';
-  }
-  else{
-    for (int g = 0; g < 256; g++) { 
-      analogWrite(GREENPIN, g);
-      delay(FADESPEED);
-    }
-    partyMode = 'r'; 
-  }
-}
-*/
 void loop(){
-  /*while(Serial.available()){//while there is data available on the serial monitor
-    if(Serial.read() == "\n"){
-      break;
-    }
-    message+=char(Serial.read());//store string from serial command
-    passed = false;
-  }*/
   while(Serial.available()){
     message += char(Serial.read());
-    if (message[message.length() - 1] == '*'){
+    if (message[message.length() - 1] == '*'){// '*' is end character for all commands sent via serial
       passed = false;
       break;
     }
   }
   if((!passed)){//will not enter condition if already entered and no new serial.
     if(message!=""){//if data is available
-      if(message.substring(0, message.length() -1).equals("ON")){//string sent from bluetooth == power
+      if(message.substring(0, message.length() -1).equals("ON")){//string sent from bluetooth
         digitalWrite(POWERPIN, LOW);
         Serial.println("ON");
       }
       else if(message.substring(0, message.length() -1).equals("OFF")){
         digitalWrite(POWERPIN, HIGH);
         Serial.println("OFF");
-      }
-      else if(message.equals("party")){
-        if(partying == true){
-          partying = false;
-        }
-        else{
-          partying = true;
-        }
       }
       else{
         r = message.substring(0,3).toInt();//reads individual rgb values from 0-255
@@ -96,9 +56,5 @@ void loop(){
       passed = true;//signifies having entered the condition.
     }
   }
-  /*
-  if(partying == true){
-    party();
-  }*/
   delay(500);
 }
