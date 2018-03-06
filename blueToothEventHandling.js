dec497ac55b0edef73876b430145ca3c56f97706
emper911 <emper96@gmail.com>
2018-03-05 19:34:47 -0500

    Update blueToothEventHandling.js

--- blueToothEventHandling.js
+++ blueToothEventHandling.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
   var redData = "000", greenData = "000", blueData = "000";
   var stat = "off", blueConnect;
@@ -62,6 +61,20 @@ $(document).ready(function(){
       }
     });
   });
+
+  $("#regLighting").on("click", function(){
+    $.ajax({
+      type: "POST",
+      url: "/index.html",
+      contentType: "application/json",
+      data: JSON.stringify({status: blueConnect, power:"null",R:"255",G:"255",B:"120"}),
+      dataType: "json",
+      success: function(){
+        $("#regLighting").css("fill", "rgb(255,241,200)");
+      }
+    });
+  });
+
   $("#redSlider").on({
     input: function(event){
       redData = $("#redSlider").val();
@@ -93,37 +106,7 @@ $(document).ready(function(){
       });
     }
    });
-   /*
-  $("#redSlider").on("input",function(event){
-    redData = $("#redSlider").val();
-    redData = redData.toString();
-    if(redData.length == 1){
-      redData = "00"+redData;
-    }
-    else if(redData.length == 2){
-      redData = "0"+redData;
-    }
-    $("#redData").text("RED: " + redData);
-    $("#colorSquare").css("fill","rgb("+redData+","+greenData+","+blueData+")");
 
-  });
-  $("#redSlider").on("click", function(event){
-    $.ajax({
-      type:"POST",
-      url:"/index.html",
-      contentType: "application/json",
-      data: JSON.stringify({identifier:"red",status: blueConnect, power:"null", R:redData, G:greenData, B:blueData}),
-      dataType: "json",
-      success: function(){
-        $("#redData").text("RED: " + redData);
-        $("#colorSquare").css("fill","rgb("+redData+","+greenData+","+blueData+")");
-      },
-      error: function(){
-        console.log("Something wrong with Red!");
-      }
-    });
-  });
-  */
   $("#greenSlider").on({
     input: function(event){
       greenData = $("#greenSlider").val();
@@ -154,35 +137,7 @@ $(document).ready(function(){
       });
     }
   });
-  /*
-  $("#greenSlider").on("input",function(event){
-    greenData = $("#greenSlider").val();
-    greenData = greenData.toString();
-    if(greenData.length == 1){
-      greenData = "00"+greenData;
-    }
-    else if(greenData.length == 2){
-      greenData = "0"+greenData;
-    }
-    $("#greenData").text("GREEN: " + greenData);
-    $("#colorSquare").css("fill","rgb("+redData+","+greenData+","+blueData+")");
-  });
-  $("#greenSlider").on("click",function(event){
-    $.ajax({
-      type:"POST",
-      url:"/index.html",
-      contentType: "application/json",
-      data: JSON.stringify({status: blueConnect, power:"null", R:redData, G:greenData, B:blueData}),
-      dataType: "json",
-      success: function(){
-        $("#greenData").text("GREEN: " + greenData);
-        $("#colorSquare").css("fill","rgb("+redData+","+greenData+","+blueData+")");
-      },
-      error: function(){
-        console.log("Something wrong with Green!");
-      }
-    });
-  });*/
+
   $("#blueSlider").on({
     input: function(event){
       blueData = $("#blueSlider").val();
@@ -213,33 +168,4 @@ $(document).ready(function(){
       });
     }
   });
-  /*
-  $("#blueSlider").on("input",function(event){
-    blueData = $("#blueSlider").val();
-    blueData = blueData.toString();
-    if(blueData.length == 1){
-      blueData = "00"+blueData;
-    }
-    else if(blueData.length == 2){
-      blueData = "0"+blueData;
-    }
-    $("#blueData").text("BLUE: " + blueData);
-    $("#colorSquare").css("fill","rgb("+redData+","+greenData+","+blueData+")");
-  });
-  $("#blueSlider").on("click",function(event){
-    $.ajax({
-      type:"POST",
-      url:"/index.html",
-      contentType: "application/json",
-      data: JSON.stringify({status: blueConnect, power:"null", R: redData, G: greenData, B: blueData}),
-      dataType: "json",
-      success: function(){
-        $("#blueData").text("BLUE: " + blueData);
-        $("#colorSquare").css("fill","rgb("+redData+","+greenData+","+blueData+")");
-      },
-      error: function(){
-        console.log("Something wrong with Blue!");
-      }
-    });
-  });*/
 });
